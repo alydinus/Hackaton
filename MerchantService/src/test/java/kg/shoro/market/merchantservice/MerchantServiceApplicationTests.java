6269fbe4e387a8180d0430e4365f3759621f7f53
alydinus <alykulovalydin@gmail.com>
2025-11-21 20:36:33 +0600

    Add initial project structure with Docker, Maven, and Spring Boot configuration

---
+++ MerchantService/src/test/java/kg/shoro/market/merchantservice/MerchantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.shoro.market.merchantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
